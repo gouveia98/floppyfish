ca6bec87521b13fd0d424add0dd45719dab4d4b9
gouveia98 <artur_gouveia_costa@hotmail.com>
2020-07-01 17:24:55 -0300

    nao sai d tela

--- actors/Player/Player.gd
+++ actors/Player/Player.gd
@@ -1,18 +1,27 @@
 extends Node2D
 
-export var max_speed = 200.0;
-export var acceleration = 50.0;
+export var MAX_SPEED = 200.0;
+export var acceleration = 400.0;
 var speed = 0.0;
 
-# Called when the node enters the scene tree for the first time.
+var margin = 16;
+onready var top_target = margin;
+onready var bottom_target = get_viewport_rect().size.y - margin;
+
 func _ready():
-	pass # Replace with function body.
+	pass
 
 func _physics_process(delta):
 	var new_accel = acceleration;
 	if Input.is_action_pressed("jump"):
 		new_accel *= -1
 	speed += new_accel*delta
+	
+	var min_speed = (-(position.y - top_target)/margin)*MAX_SPEED
+	min_speed = clamp(min_speed, -MAX_SPEED, MAX_SPEED);
+	var max_speed = (-(position.y - bottom_target)/margin)*MAX_SPEED
+	max_speed = clamp(max_speed, -MAX_SPEED, MAX_SPEED)
+	speed = clamp(speed, min_speed, max_speed)
+	
 	position.y += speed*delta
-	speed = clamp(speed, -max_speed, max_speed)
-	rotation = speed*0.7/max_speed
+	rotation = atan2(speed, MAX_SPEED)
